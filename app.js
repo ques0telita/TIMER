ce0eea21f1a9c8784e8ea6e497ed73ecc3a2d76e
Sebastian <sebastianrcm06@gmail.com>
2026-05-05 00:50:40 -0400

    timre fixed

--- app.js
+++ app.js
@@ -3,39 +3,37 @@ let intervalo;
 const pantalla = document.getElementById("pantalla");
 const btn = document.getElementById("start-btn");
 const videoBoom = document.getElementById("video-boom");
-const videoFondo = document.getElementById("video-fondo");
+const numberInput = document.getElementById("number");
 
 btn.addEventListener('click', () => {
-// se limpian los intervalos previos
-if (intervalo) clearInterval(intervalo);
+    if (intervalo) clearInterval(intervalo);
 
-// reset
-let contador = 60;
-pantalla.innerText = contador;
-btn.disabled = true;
+    const valorInput = parseInt(numberInput.value, 10);
+    const contadorInicial = Number.isNaN(valorInput) 
+    ? 10 : valorInput;
+
+    let contador = contadorInicial;
+    pantalla.innerText = contador;
+    btn.disabled = true;
+
 videoBoom.style.display = "none";
-videoBoom.pause();
 videoBoom.currentTime = 0;
 
-// 
 intervalo = setInterval(() => {
-contador--;
+contador --;
 
 if (contador > 0) {
     pantalla.innerText = contador;
 } else {
-    // se muestra el mensaje
     clearInterval(intervalo);
-    pantalla.innerText = "LUNCH!";
+    pantalla.innerText = "LAUNCH!"; //
     
-    // al darle al boton de reinicio, se muestra otra vez el video del despegue
     videoBoom.style.display = "block";
-    videoBoom.play().catch(error => console.log("El navegador bloqueó el video:", error));
-    
-    btn.disabled = false;
-    btn.innerText = "REINTENTAR";
-    videoBoom.end().then(replay)
+    videoBoom.play().catch(error => console.log("Error:", error));
 
+    btn.disabled = false;
+    btn.innerText = "RESET";
+    
 }
 }, 1000);
 });
